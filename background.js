de3f0f8bab9598ec5f1145d479aa326fa59fd5a1
fliuleidenuniv-create <fliu.leidenuniv@gmail.com>
2025-10-10 16:19:10 +0200

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  // Show the side panel toggle in the toolbar by default
-  chrome.sidePanel.setOptions({ path: 'panel.html', enabled: true });
-});
-
-chrome.tabs.onActivated.addListener(async () => {
-  // Keep the same panel UI across tabs
-  await chrome.sidePanel.setOptions({ path: 'panel.html', enabled: true });
-});
